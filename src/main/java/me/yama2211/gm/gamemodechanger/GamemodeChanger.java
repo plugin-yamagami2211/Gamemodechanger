0d41c556c57fa1961634936123742fdb1a3b20f8
yamagami2211 <yamagami2211@svk.jp>
2020-08-05 16:35:07 +0900

    v1.0

--- src/main/java/me/yama2211/gm/gamemodechanger/GamemodeChanger.java
+++ src/main/java/me/yama2211/gm/gamemodechanger/GamemodeChanger.java
@@ -1,6 +1,11 @@
+/*
+*
+* GamemodeChanger
+*
+* */
+
 package me.yama2211.gm.gamemodechanger;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
@@ -23,88 +28,62 @@ public final class GamemodeChanger extends JavaPlugin {
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
     {
-        if(sender instanceof Player){
+        if(sender instanceof Player) {
             Player player = (Player) sender;
-            String targetName = args[1];
-            Player targetPlayer = Bukkit.getPlayer(targetName);
-            /* /gm <0|1|2|3> */
-            if(targetPlayer == null){
-                /*サバイバル*/
-                if(cmd.getName().equalsIgnoreCase("0")){
-                    if(player.hasPermission("gamemode.survival")){
-                        player.setGameMode(GameMode.SURVIVAL);
-                        player.sendMessage(ChatColor.translateAlternateColorCodes('&',getConfig().getString("Gamemode"+".Survival")));
-                    }
-            }
-                /*クリエイティブ*/
-                if(cmd.getName().equalsIgnoreCase("1")){
-                    if(player.hasPermission("gamemode.creative")){
-                        player.setGameMode(GameMode.CREATIVE);
-                        player.sendMessage(ChatColor.translateAlternateColorCodes('&',getConfig().getString("Gamemode"+".Creative")));
-                    }
-            }
-                /*アドベンチャー*/
-                if(cmd.getName().equalsIgnoreCase("2")){
-                    if(player.hasPermission("gamemode.adventure")){
-                        player.setGameMode(GameMode.ADVENTURE);
-                        player.sendMessage(ChatColor.translateAlternateColorCodes('&',getConfig().getString("Gamemode"+".Adventure")));
-                    }
+
+
+            if (cmd.getName().equalsIgnoreCase("gm")) {
+                if (args.length == 0) {
+                    player.sendMessage(ChatColor.RED + "/gm <0|1|2|3> [playername]");
                 }
 
-                /*スペクテイター*/
-                if(cmd.getName().equalsIgnoreCase("3")){
-                    if(player.hasPermission("gamemode.spectator")){
-                    player.setGameMode(GameMode.SPECTATOR);
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&',getConfig().getString("Gamemode"+".Spectator")));
-                    }
-                }
-        }
-            /* /gm <0|1|2|3> [playername] */
-            if(cmd.getName().equalsIgnoreCase("0")){
-                /* サバイバル */
-                if(player.hasPermission("gamemode.survival")) {
-                    targetPlayer.setGameMode(GameMode.SURVIVAL);
-                    String ChSu = getConfig().getString("Gamemode" + ".Player" + ".Survival");
-                    ChSu = ChSu.replaceAll("%player", targetPlayer.getName());
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', ChSu));
-                    targetPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', ChSu));
-                }
-            }
+                if (args.length == 1) {
+                        /*サバイバル*/
+                        if (args[0].equalsIgnoreCase("0")) {
+                            if (!player.hasPermission("gamemode.survival")) {
+                                player.sendMessage(ChatColor.RED + "権限がありません。");
+                            }
+                            else{
+                                player.setGameMode(GameMode.SURVIVAL);
+                                player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Gamemode" + ".Survival")));
+                            }
+                        }
+                        /*クリエイティブ*/
+                        if (args[0].equalsIgnoreCase("1")) {
+                            if (!player.hasPermission("gamemode.creative")) {
+                                player.sendMessage(ChatColor.RED + "権限がありません。");
+                            }
+                            else{
+                                player.setGameMode(GameMode.CREATIVE);
+                                player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Gamemode" + ".Creative")));
+                            }
+                        }
+                        /*アドベンチャー*/
+                        if (args[0].equalsIgnoreCase("2")) {
+                            if (!player.hasPermission("gamemode.adventure")) {
+                                player.sendMessage(ChatColor.RED + "権限がありません。");
+                            }
+                            else{
+                                player.setGameMode(GameMode.ADVENTURE);
+                                player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Gamemode" + ".Adventure")));
+                            }
+                        }
 
-            if(cmd.getName().equalsIgnoreCase("1")){
-                /* クリエイティブ */
-                if(player.hasPermission("gamemode.creative")) {
-                    targetPlayer.setGameMode(GameMode.CREATIVE);
-                    String ChCr = getConfig().getString("Gamemode" + ".Player" + ".Creative");
-                    ChCr = ChCr.replaceAll("%player", targetPlayer.getName());
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', ChCr));
-                    targetPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', ChCr));
-                }
-            }
+                        /*スペクテイター*/
+                        if (args[0].equalsIgnoreCase("3")) {
+                            if (!player.hasPermission("gamemode.spectator")) {
+                                player.sendMessage(ChatColor.RED + "権限がありません。");
+                            }
+                            else{
+                                player.setGameMode(GameMode.SPECTATOR);
+                                player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Gamemode" + ".Spectator")));
+                            }
+                        }
+                }//args 1
+            }//cmd.getName gm
 
-            if(cmd.getName().equalsIgnoreCase("2")){
-                /* アドベンチャー */
-                if(player.hasPermission("gamemode.adventure")) {
-                    targetPlayer.setGameMode(GameMode.ADVENTURE);
-                    String ChAd = getConfig().getString("Gamemode" + ".Player" + ".Adventure");
-                    ChAd = ChAd.replaceAll("%player", targetPlayer.getName());
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', ChAd));
-                    targetPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', ChAd));
-                }
-            }
 
-            if(cmd.getName().equalsIgnoreCase("3")){
-                /* スペクテイター */
-                if(player.hasPermission("gamemode.spectator")) {
-                    targetPlayer.setGameMode(GameMode.SPECTATOR);
-                    String ChSp = getConfig().getString("Gamemode" + ".Player" + ".Spectator");
-                    ChSp = ChSp.replaceAll("%player", targetPlayer.getName());
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', ChSp));
-                    targetPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', ChSp));
-                }
-            }
-
-        }
+        }//sender instanceof Player
 
         return true;
     }
